68b95f39bfaf2753024196faaccb62642976e852
kotaoshida <oshi.kt@icloud.com>
2021-05-08 20:30:37 +0900

    make it better

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@
 const mysql  = require('mysql2');
 
 const db = mysql.createConnection({
-  host     : 'localhost',
+  host     : 'db',
   user     : "root",
   password : '',
   database : 'socialmedia'
