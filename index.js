c6b4ec6a79602065f67f31eecd22eaab670bb102
kotaoshida <oshi.kt@icloud.com>
2021-05-08 20:59:24 +0900

    make it better

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require("express");
 const cors = require('cors')
 const app = express()
 
-const fs = require('fs')
-const https = require('https')
 
 
 app.use(cors())
 
 app.use(express.json());
+
+const PORT = "3001"
 
 const userRoute =require("./route/user");
 app.use("/user",userRoute);
@@ -16,11 +16,7 @@ app.use("/user",userRoute);
 const uploadRoute =require("./route/upload");
 app.use("/upload",uploadRoute);
 
-https.createServer({
-  key: fs.readFileSync("/Users/kotaoshida/key.pem"),
-  cert: fs.readFileSync('/Users/kotaoshida/cert.pem')
-}, app)
-.listen(3001, function () {
+app.listen(process.env.PORT||PORT, function () {
   console.log('Example app listening on port 3000! Go to https://localhost:3000/')
 })
 
